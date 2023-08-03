eabedc83877e736f7b30430368e950905d775e4c
Santosh Tuniki <tunikisantosh123@gmail.com>
2023-08-03 15:08:35 +0530

    SIGTERM & uncaughtExceptions handling

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,7 +7,37 @@ import logger from './configs/logger.config.js';
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV)
 
-// Listen on port
-app.listen(PORT, () => {
-    logger.info(`Server is listening to PORT ${PORT}...`)
+let server;
+
+// listen on port
+server = app.listen(PORT, () => {
+    logger.info(`Server is listening to PORT ${PORT}...`);
+    console.log('process id: ', process.pid);
+    // throw new Error('error in server..')
+})
+
+// handle server errors
+const exitHandler = () => {
+    if(server){
+        logger.info('Server closed');
+        process.exit(1);
+    }else{
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error.stack);
+    exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+// SIGTERM
+process.on('SIGTERM', () => {
+    if(server){
+        logger.info('Server closed');
+        process.exit(1);
+    }
 })
