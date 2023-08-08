9fb8e3030617525d75585f37c8eba6ecb7c881b0
Santosh Tuniki <tunikisantosh123@gmail.com>
2023-08-08 11:20:54 +0530

    Handling picture

--- Frontend/whatsapp/src/components/auth/Picture.jsx
+++ Frontend/whatsapp/src/components/auth/Picture.jsx
@@ -1,9 +1,91 @@
 // module imports
+import { useRef, useState } from 'react';
 
-function Picture() {
+function Picture({ readablePicture, setReadablePicture, setPicture }) {
+
+    const [error, setError] = useState('');
+
+    const inputRef = useRef();
+
+    const handlePicture = (e) => {
+        const pic = e.target.files[0];
+        if (
+            pic.type !== 'image/png' &&
+            pic.type !== 'image/jpg' &&
+            pic.type !== 'image/jpeg' &&
+            pic.type !== 'image/webp'
+        ) {
+            setError(`${pic.name} format is not supported.`);
+            return;
+        }
+        else if (pic.size > 1024 * 1024 * 5) {  // 5MB
+            setError(`${pic.name} is too large, maximum 5 MB allowed.`);
+            return;
+        }
+        else {
+            setError('');
+            setPicture(pic);
+
+            // reading the picture
+            const reader = new FileReader();
+            reader.readAsDataURL(pic);
+            reader.onload = (e) => {
+                setReadablePicture(e.target.result)
+            }
+        }
+    }
+
+    const handleChangePic = () => {
+        // reset everything
+        setPicture('');
+        setReadablePicture('');
+    }
+
     return (
-        <div className="mt-8 content-center dark:text-dark_text_1 ">
-            Picture
+        <div className='mt-8 content-center dark:text-dark_text_1'>
+            <label htmlFor='picture' className='text-sm font-bold tracking-wide'>
+                Picture ( Optional )
+            </label>
+            {/* Upload Button */}
+            {
+                readablePicture ? (
+                    <div>
+                        <img
+                            src={readablePicture}
+                            alt='picture'
+                            className='w-20 h-20 object-cover rounded-full'
+                        />
+                        {/* Change Pic */}
+                        <div
+                            onClick={() => handleChangePic()}
+                            className='mt-2 w-20 py-2 dark:bg-dark_bg_3 rounded-md text-xs font-bold flex items-center justify-center cursor-pointer'
+                        >
+                            Remove
+                        </div>
+                    </div>
+                ) : (
+                    <div
+                        onClick={() => inputRef.current.click()}
+                        className='w-full h-12 dark:bg-dark_bg_3 rounded-md font-bold flex items-center justify-center cursor-pointer'
+                    >
+                        Upload Picture
+                    </div>
+                )
+            }
+            {/* input upload */}
+            <input
+                type='file' name='picture' id='picture' hidden ref={inputRef}
+                accept='image/png, image/jpeg, image/jpg, image/webp'
+                onChange={handlePicture}
+            />
+            {/* error */}
+            {
+                error ? (
+                    <div>
+                        <p className='text-red-400'>{error}</p>
+                    </div>
+                ) : null
+            }
         </div>
     )
 }
--- Frontend/whatsapp/src/components/auth/RegisterForm.jsx
+++ Frontend/whatsapp/src/components/auth/RegisterForm.jsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { signUpSchema } from '../../utils/validation';
 import AuthInput from './AuthInput';
 import { registerUser } from '../../features/userSlice';
+import Picture from './Picture';
 
 function RegisterForm() {
 
@@ -44,7 +45,7 @@ function RegisterForm() {
 	// console.log('errors: ', errors);
 
 	return (
-		<div className='p-10 w-full flex items-center justify-center overflow-hidden'>
+		<div className='min-h-full w-full flex items-center justify-center overflow-hidden'>
 			{/* Container */}
 			<div className='w-full max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl'>
 				{/* Heading */}
@@ -71,7 +72,7 @@ function RegisterForm() {
 					<AuthInput
 						name='status'
 						type='text'
-						placeholder='Status (Optional)'
+						placeholder='Status ( Optional )'
 						register={register}
 						error={errors?.status?.message}
 					/>
@@ -83,7 +84,11 @@ function RegisterForm() {
 						error={errors?.password?.message}
 					/>
 					{/* picture */}
-
+					<Picture
+						readablePicture={readablePicture}
+						setReadablePicture={setReadablePicture}
+						setPicture={setPicture}
+					/>
 					{/* if we have an error */}
 					{
 						error ? (
@@ -95,7 +100,7 @@ function RegisterForm() {
 					{/* Submit Button */}
 					<button
 						type='submit'
-						className='w-full flex justify-center bg-green_1 text-gray-100 rounded-full tracking-wide py-2
+						className='w-full flex justify-center bg-green_1 text-gray-100 rounded-full tracking-wide py-3
 						font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300'
 					>
 						{status === 'loading' ? <PulseLoader color='#fff' size={16} /> : 'Sign up'}
