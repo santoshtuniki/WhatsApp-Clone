3d82490571794430f122ba60507031e8d6a64bb9
Santosh Tuniki <tunikisantosh123@gmail.com>
2023-08-29 19:06:02 +0530

    call header added

--- Frontend/whatsapp/src/components/chat/call/Call.jsx
+++ Frontend/whatsapp/src/components/chat/call/Call.jsx
@@ -1,11 +1,26 @@
 // component imports
-import { Ringing } from './index';
+import { Ringing, Header } from './index';
 
 function Call({ call, setCall, callAccepted }) {
     const { receivingCall, callEnded } = call;
 
     return (
-        <div>
+        <div
+            className={
+                `fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[550px] z-10 rounded-2xl overflow-hidden callbg
+                ${receivingCall && !callAccepted ? 'hidden' : ''}
+            `}
+        >
+            {/*Container*/}
+            <div>
+                <div>
+                    {/*Header*/}
+                    <Header />
+
+                </div>
+            </div>
+
+            {/*Ringing*/}
             {
                 receivingCall && !callAccepted && (
                     <Ringing call={call} setCall={setCall} />
--- Frontend/whatsapp/src/components/chat/call/index.js
+++ Frontend/whatsapp/src/components/chat/call/index.js
@@ -1,9 +1,11 @@
 // component imports
 import Ringing from './Ringing';
 import Call from './Call';
+import Header from './Header';
 
 // named exports
 export {
     Ringing,
     Call,
+    Header,
 }
